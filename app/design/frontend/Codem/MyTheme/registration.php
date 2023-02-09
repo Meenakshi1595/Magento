689178353b787d258be4c8bee61e071244234a30
Meenakshi1595 <meenuks1515@gmail.com>
2023-02-09 20:14:36 +0530

    Adding magento

---
+++ app/design/frontend/Codem/MyTheme/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Codem/MyTheme',
+    __DIR__
+);
---
+++ app/etc/env.php
@@ -0,0 +1,82 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_1p0yyt'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '13248faa4777f60fd450adf82e9f217b'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1:8889',
+                'dbname' => 'magento_first',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '34a_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '34a_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'magento.localhost'
+    ],
+    'install' => [
+        'date' => 'Wed, 01 Feb 2023 07:32:24 +0000'
+    ]
+];
